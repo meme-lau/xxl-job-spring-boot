b67eb7548ca79c03e66adfa71bce38aa587644af
meme <meme@memestudio.dev>
2020-06-10 11:46:32 +0800

    upgrade 2.2

--- xxl-job-spring-boot-autoconfigure/src/main/java/ml/memelau/xxl/job/spring/boot/autoconfigure/XxlJobAutoConfiguration.java
+++ xxl-job-spring-boot-autoconfigure/src/main/java/ml/memelau/xxl/job/spring/boot/autoconfigure/XxlJobAutoConfiguration.java
@@ -2,6 +2,7 @@ package ml.memelau.xxl.job.spring.boot.autoconfigure;
 
 import com.xxl.job.core.executor.XxlJobExecutor;
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
  * @author meme
  * @since 2019-03-04 18:05
  */
+@Slf4j
 @Configuration
 @ConditionalOnClass(XxlJobExecutor.class)
 @EnableConfigurationProperties(XxlJobProperties.class)
@@ -24,8 +26,9 @@ public class XxlJobAutoConfiguration {
     @ConditionalOnMissingBean
     @Bean(initMethod = "start", destroyMethod = "destroy")
     public XxlJobExecutor xxlJobExecutor(XxlJobProperties properties){
+        log.info(">>>>>>>>>>> xxl-job config init.");
         XxlJobExecutor xxlJobExecutor = new XxlJobSpringExecutor();
-        xxlJobExecutor.setAppName(properties.getExecutor().getAppName());
+        xxlJobExecutor.setAppname(properties.getExecutor().getAppName());
         xxlJobExecutor.setPort(properties.getExecutor().getPort());
         xxlJobExecutor.setIp(properties.getExecutor().getIp());
         xxlJobExecutor.setAdminAddresses(String.join(",", properties.getAdmin().getAddresses()));
